ebcd122eead6a01c81ec1db9cb5d5f8af2cb31a8
anastasiasch30 <anastasia.sch30@gmail.com>
2023-11-17 21:02:46 +0100

    Code cleaner sans commentaires intempestifs

--- v2_Code.py
+++ v2_Code.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov  1 21:06:55 2023
+Python project due November 17th 2023
 
-@author: anast
+@author: Anastasia and Eddy
 """
-#NB need prints somewhere!!
-#Save all relevant graphs
-
-#Harmoniser les " et ', mettre que des " car ' est utilisé en grammaire anglaise
 
 import pandas as pd 
 import numpy as np
@@ -33,18 +29,18 @@ churn_data.isnull().sum()
 
 
 
-
 ##### Getting to know the dataset
 
-print(churn_data.head())
+##Setting parameters for graphical representations
+#Setting our favorite colours: 
+colors = ["#FFD699", "#87CEEB"]
+#To separate the components of our pie charts 
+explode = (0.1, 0)  
 
 col_names = churn_data.columns.values.tolist()
 print("There are ",len(churn_data), " customers in the data set and there are ", len(col_names), "features.")
-#churn_data.columns() This is an index object, not a list, so can't just print it
 
 #Surname
-#np.unique(churn_data.Surname)
-#len(np.unique(churn_data.Surname)) #2932 surprisingly low!
 print("There are ", len(np.unique(churn_data.Surname)), "unique last names in the dataset." )
 
 #Surnames could be an indicator of families. However, there are too many different surnames, some of which are common and with over 32 customers with the same last name.
@@ -56,25 +52,17 @@ print("There are between ", min(freq_surname), " and ", max(freq_surname), " cus
 
 
 #Credit score
-#Je préfère le seaborn je pense
-#plt.hist(churn_data.CreditScore)
 print("The average credit score is ", np.mean(churn_data.CreditScore))
-#np.median(churn_data.CreditScore)
 plt.figure(figsize=(5.5, 3.5))
-sns.kdeplot(churn_data['CreditScore'], fill=True, color='skyblue', linewidth=2)
+sns.kdeplot(churn_data["CreditScore"], fill=True, color="skyblue", linewidth=2)
+plt.show()
 
-#Setting our favorite colours: 
-colors = ['#FFD699', '#87CEEB']
 
-#To separate the components of our pie charts 
-
-explode = (0.1, 0)  
 #Geography
-
 countries = np.unique(churn_data.Geography)
 freq_country = [sum(churn_data.Geography == i) for i in countries]
-#Better graph?
-plt.bar(countries,freq_country)
+sns.barplot(countries,freq_country, color=colors[1])
+plt.show()
 for i in range(len(countries)):
     print("There are ",freq_country[i], "customers in "+countries[i])
 
@@ -84,74 +72,70 @@ gender = np.unique(churn_data.Gender)
 freq_gender = [sum(churn_data.Gender == i ) for i in gender]
 
 plt.pie(freq_gender, labels = gender, autopct="%1.1f%%", startangle =90, colors=colors, explode = explode, shadow=True)
+plt.show()
 print(round(freq_gender[0]/100,1), "% of the customers are female. The rest are male.")
 
 
 #Age
-#plt.hist(churn_data.Age)
 plt.figure(figsize=(5.5, 3.5))
-sns.kdeplot(churn_data['Age'], fill=True, color='skyblue', linewidth=2)
+sns.kdeplot(churn_data["Age"], fill=True, color="skyblue", linewidth=2)
+plt.show()
 
-#np.mean(churn_data.Age)
-#np.median(churn_data.Age)
 print("The average age of a customer in the data set is ",round(np.mean(churn_data.Age)), " years old")
+
 
 #Tenure
 freq_tenure = [sum(churn_data.Tenure == i ) for i in range(11)]
-plt.bar(np.unique(churn_data.Tenure),freq_tenure) 
-#sns.kdeplot(churn_data.Tenure) Not as relevant here because nb of years is discrete
+sns.barplot(np.unique(churn_data.Tenure),freq_tenure, color=colors[1]).set(title = "Distribution of customers by number of years of tenure")
+plt.show()
 print("The average customer has been at this bank for ", np.mean(churn_data.Tenure) )
-#Important to note that there is no cutoff
 
 
 #Balance
-#plt.hist(churn_data.Balance) #A: Large number with very low
-
-#sns.kdeplot(churn_data.Balance).set(title = "Distribution of bank balance of customers")
-sns.kdeplot(churn_data.Balance, fill=True, color='skyblue', linewidth=2)
-
-#Seems to have a normal distribution, with the exception of the low balance?
+sns.kdeplot(churn_data.Balance, fill=True, color="skyblue", linewidth=2)
 print("The median bank balance is", np.median(churn_data.Balance))
 #Median because of the bimodal distribution
 
+
 #NumOfProducts
 freq_NumOfProducts = [sum(churn_data.NumOfProducts == i ) for i in range(1,5)]
-#plt.bar(range(1,5), freq_NumOfProducts) #Make this 1, 2, or 3 or 4, not 0.4
-plt.pie(freq_NumOfProducts, labels = range(1,5), autopct='%1.1f%%') #Maybe not ideal...
+plt.pie(freq_NumOfProducts, labels = range(1,5), startangle =90, colors=colors, explode = (0.1, 0,0,0), shadow=True)
+plt.show()
 print(round(freq_NumOfProducts[0]/100), "% of customers have one bank product. ",round(freq_NumOfProducts[1]/100), "% of customers have 2 products.")
 
 
 #HasCrCard
 label_HasCrCard = ["Has a credit card", "Does not have a credit card"]
-freq_HasCrCard= [sum(churn_data.HasCrCard), 10000-sum(churn_data.HasCrCard) ] #A: OPTIMIZE!!!!!
-
+freq_HasCrCard= [sum(churn_data.HasCrCard), 10000-sum(churn_data.HasCrCard) ] 
 plt.pie(freq_HasCrCard, labels = label_HasCrCard, autopct="%1.1f%%", startangle = 90,  colors=colors, explode = explode, shadow=True)
+plt.show()
 print(round(freq_HasCrCard[0]/100), "% of customers have a credit card. ",round(freq_HasCrCard[1]/100), "% of customers do not have a credit card.")
 
 
 #IsActiveMember
 label_IsActiveMember = ["Active member", "Inactive member"]
-freq_IsActiveMember= [sum(churn_data.IsActiveMember), 10000-sum(churn_data.IsActiveMember) ] #A: OPTIMIZE!!!!!
+freq_IsActiveMember= [sum(churn_data.IsActiveMember), 10000-sum(churn_data.IsActiveMember) ]
 
 plt.pie(freq_IsActiveMember, labels = label_IsActiveMember, autopct="%1.1f%%", startangle = 90,  colors=colors, explode = explode, shadow=True)
 plt.title("Proportion of bank customers that are active")
+plt.show()
 print(round(freq_IsActiveMember[0]/100), "% of customers are active. ",round(freq_IsActiveMember[1]/100), "% of customers are not active.")
 
 
 #EstimatedSalary
-#plt.hist(churn_data.EstimatedSalary) #A: very ugly, find smth better
-sns.kdeplot(churn_data.EstimatedSalary).set(title = "Distribution of customers' estimated salary") #Relatively homogenous
-print("The average customer's estimated yearly salary is "+("{:,}".format(round(np.mean(churn_data.EstimatedSalary)))) + " euros.") #Put less decimal points
+sns.kdeplot(churn_data.EstimatedSalary, fill=True, color="skyblue", linewidth=2).set(title = "Distribution of customers' estimated salary")
+plt.show()
+print("The average customer's estimated yearly salary is "+("{:,}".format(round(np.mean(churn_data.EstimatedSalary)))) + " euros.") 
 
 
 ##### What is the churn rate for the bank customers?
-#/!\ For statistical significance, see below Eddy's stat tests
 
-#the annual percentage rate at which customers leave
+#The annual percentage rate at which customers leave
 churn_rate = sum(churn_data.Exited)/len(churn_data)
 print("The churn rate for all of the customers is ", round(churn_rate*100), "%")
 
-sns.countplot(data=churn_data, x="Exited", hue="Gender") 
+sns.countplot(data=churn_data, x="Exited", hue="Gender", palette = colors*2) 
+plt.show()
 
 churn_rate_genderF = sum(churn_data.Exited[churn_data.Gender == "Female"])/len(churn_data[churn_data.Gender == "Female"])
 churn_rate_genderM = sum(churn_data.Exited[churn_data.Gender == "Male"])/len(churn_data[churn_data.Gender == "Male"])
@@ -176,12 +160,12 @@ churn_data_v2["Gender"] = churn_data_v2["Gender"].replace({"Female": 1, "Male": 
 correlation_matrix = churn_data_v2.corr()
 print(correlation_matrix) 
 
-#Pretty covariance matrix, maybe see about the colors
-
+#Illustrative covariance matrix
 mask = np.triu(np.ones_like(correlation_matrix, dtype=bool))
 f, ax = plt.subplots(figsize=(11, 9))
-cmap = sns.diverging_palette(230, 20, as_cmap=True)
-sns.heatmap(correlation_matrix, mask=mask, cmap=cmap, vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
+cmap_custom = sns.diverging_palette(230, 20, as_cmap=True)
+sns.heatmap(correlation_matrix, mask=mask, cmap=cmap_custom, vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
+plt.show()
 
 
 ### For the age variable 
@@ -189,32 +173,30 @@ sns.heatmap(correlation_matrix, mask=mask, cmap=cmap, vmax=.3, center=0, square=
 plt.xlim(16, 80)
 sns.lineplot(x="Age", y="IsActiveMember", data= churn_data_v2, marker="o", color="blue")
 plt.ylabel("Client activity")
+plt.show()
 
 plt.xlim(16, 80)
 sns.lineplot(x="Age", y="Exited", data=churn_data_v2, marker="o", color="blue")
 plt.ylabel("Churn Rate")
+plt.show()
+
 
 ##Geography
 
 #Balance distribution across countries
 sns.boxplot(y="Balance",x = "Geography", hue = "Geography",data = churn_data_v2).set(xlabel="")
 plt.gca().legend().remove()
+plt.show()
 
 sns.countplot(data=churn_data_v2, x="Exited", hue="Geography")
+plt.show()
 
 
+### Influence of other variables on churn 
+sns.countplot(data=churn_data_v2, x="Exited", hue="Gender") 
 
 
-### Influence of other variables on churn --> je ne comprends pas ce que tu veux faire?
-sns.countplot(data=churn_data_v2, x="Exited", hue="Gender") #Refaire ça avec les variables au début (avec female et male plutôt)
-
-#Higher churn rate for female compared to male
-#Higher churn rate in Germany and spain compared to france, maybe a link with the number of clients
-
-#/!\ An: tu ne calcules pas le churn rate, juste le nb de exits?
-#Je réflechis à qqch de plus parlant?
-
-#Are they significative ? 
+#Are the differences statistically significant ? 
 from scipy.stats import ttest_ind
 
 #Separate the churn variable according to gender and geography
@@ -227,24 +209,19 @@ germany_data= churn_data_v2[churn_data_v2["Geography"] == "Germany"]["Exited"]
 
 #For gender
 t_statistic, p_value = ttest_ind(female_data, male_data)
-print(f"T-statistic: {t_statistic}")
-print(f"P-value: {p_value}")
+print("The difference in churn rate between men and women is statistically significant: ",  f"P-value is {p_value} and ", f"T-statistic: {t_statistic}")
 
 #For geography 
 t_statistic, p_value = ttest_ind(france_data, spain_data)
-print(f"T-statistic: {t_statistic}")
-print(f"P-value: {p_value}")
+print("The difference in churn rate between France and Spain is not statistically significant: ",  f"P-value is {p_value} and ", f"T-statistic is {t_statistic}")
 #Not significant
 
 t_statistic, p_value = ttest_ind(france_data, germany_data)
-print(f"T-statistic: {t_statistic}")
-print(f"P-value: {p_value}")
-#p<0.001
+print("The difference in churn rate between France and Germany is statistically significant: ",  f"P-value is {p_value} and ", f"T-statistic is {t_statistic}")
 
 t_statistic, p_value = ttest_ind(spain_data, germany_data)
-print(f"T-statistic: {t_statistic}")
-print(f"P-value: {p_value}")
-#p<0.001
+print("The difference in churn rate between Spain and Germany is statistically significant: ",  f"P-value is {p_value} and ", f"T-statistic is {t_statistic}")
+
 
 #We run a probit regression to look at:
     #1) which variables have a statistically significant impact on churn
@@ -255,15 +232,17 @@ from statsmodels.tools.tools import add_constant
 
 churn_data_v2["Geography"] = churn_data_v2["Geography"].replace({"France": 1, "Spain": 2, "Germany":3})
 
-#print(churn_data.head())
-#churn_data.describe()
-
 Y = churn_data_v2["Exited"]
 X = churn_data_v2.drop(["Exited"], 1)
-X = add_constant(X) #Att why this?
+X = add_constant(X) 
 model = Probit(Y, X.astype(float))
 probit_model = model.fit()
 print(probit_model.summary())
+
+
+
+
+
 
 
 ### Machine learning Model ###
@@ -280,7 +259,7 @@ churn_data_ml["Gender"] = churn_data_ml["Gender"].replace({"Female": 1, "Male": 
 churn_data_ml = churn_data_ml.drop(["CustomerId", "Surname"], axis = 1)
 
 #Adding dummies for the presence or not of one country in the row 
-churn_data_ml = pd.get_dummies(churn_data_ml, columns=["Geography"], prefix='geo', drop_first=False)
+churn_data_ml = pd.get_dummies(churn_data_ml, columns=["Geography"], prefix="geo", drop_first=False)
 
 Y = churn_data_ml["Exited"]
 X = churn_data_ml.drop("Exited", axis = 1)
@@ -303,4 +282,3 @@ accuracy_best = accuracy_score(Y_test, Y_predicted)
 
 print("The best parameters:", grid_search.best_params_)
 print(f"Accuracy (Best Model): {accuracy_best:.4f}")
-
