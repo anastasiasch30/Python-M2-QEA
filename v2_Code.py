1bd07e0e24f427e99ea739d8eb7f8b84d7aa8bfd
Douwan12 <135887014+Douwan12@users.noreply.github.com>
2023-11-17 18:41:30 +0100

    Update v2_Code.py

--- v2_Code.py
+++ v2_Code.py
@@ -146,6 +146,8 @@ print("The average customer's estimated yearly salary is "+('{:,}'.format(round(
 churn_rate = sum(churn_data.Exited)/len(churn_data)
 print("The churn rate for all of the customers is ", round(churn_rate*100), "%")
 
+sns.countplot(data=churn_data, x="Exited", hue="Gender") 
+
 churn_rate_genderF = sum(churn_data.Exited[churn_data.Gender == "Female"])/len(churn_data[churn_data.Gender == "Female"])
 churn_rate_genderM = sum(churn_data.Exited[churn_data.Gender == "Male"])/len(churn_data[churn_data.Gender == "Male"])
 print("The churn rate for women is ", round(churn_rate_genderF*100), "%")
@@ -156,71 +158,44 @@ for i in countries:
     print("The churn rate for "+i+" is ", round(churn_rate_country*100), "%")
 
 
-#Now we drop it?
-#/!\ Why?
-#Att: change the name!!!!
-churn_data = churn_data.drop(["CustomerId", "Surname"], axis = 1) 
-#Att: need to get rid of Surname for the probit, so I kept it here
-#Should probs give it another name
+#Dropping the two non-explicative variables
+
+churn_data_v2 = churn_data.drop(["CustomerId", "Surname"], axis = 1) 
+
 
 ##### What is the relationships between variables ? 
 #Replacing gender by numerical values 
-churn_data["Gender"] = churn_data["Gender"].replace({"Female": 1, "Male": 0})
+churn_data_v2["Gender"] = churn_data_v2["Gender"].replace({"Female": 1, "Male": 0})
 
 #simple covariance matrix
-correlation_matrix = churn_data.corr()
+correlation_matrix = churn_data_v2.corr()
 print(correlation_matrix) 
-#In general what we can remark is that explicative variables are not that much correlated with one another, 
-#which is good thing since it implies more explicative power for the churn rate.
-
 
 #Pretty covariance matrix, maybe see about the colors
 
 mask = np.triu(np.ones_like(correlation_matrix, dtype=bool))
 f, ax = plt.subplots(figsize=(11, 9))
-
 cmap = sns.diverging_palette(230, 20, as_cmap=True)
-
 sns.heatmap(correlation_matrix, mask=mask, cmap=cmap, vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
 
 
-
 ### For the age variable 
+ 
+plt.xlim(16, 80)
+sns.lineplot(x='Age', y='IsActiveMember', data= churn_data_v2, marker='o', color='blue')
+plt.ylabel("Client activity")
 
-#Activity 
-sns.lineplot(x="Age", y="IsActiveMember", data=churn_data, marker="o")
-#I like th
+plt.xlim(16, 80)
+sns.lineplot(x='Age', y='Exited', data=churn_data_v2, marker='o', color='blue')
+plt.ylabel("Churn Rate")
 
+##Geography
 
-##For all other variables, the mean stays constant, for Balance and Estimated Salary it is 
-###What is the distribution of variables across countries ?
-#Clients count
-country_count = churn_data.groupby("Geography").size().reset_index(name="Count")
-custom_colors = ["blue", "green", "orange"]
-
-#Att: ne marche pas, grouped_data n'a pas d'attribut Geography et n'est défini qu'après
-#plt.bar(grouped_data['Geography'], grouped_data['Count'], color = custom_colors)
-#plt.ylabel('Number of clients')
-
-
-#Suggestion An: est-ce que c'est ce que tu voulais? On l'avait déjà au dessus je crois
-plt.bar(country_count["Geography"], country_count["Count"], color = custom_colors)
-plt.ylabel("Number of clients")
-
-#Distributions
-grouped_data = churn_data.groupby("Geography").mean().reset_index()
-
-sns.boxplot(y="CreditScore", x="Geography", hue="Geography", data=churn_data).set(xlabel='')
+#Balance distribution across countries
+sns.boxplot(y="Balance",x = "Geography", hue = "Geography",data = churn_data_v2).set(xlabel='')
 plt.gca().legend().remove()
 
-sns.boxplot(y="Tenure",x = "Geography", hue = "Geography", data = churn_data).set(xlabel='')
-plt.gca().legend().remove()
-
-sns.boxplot(y="Balance",x = "Geography", hue = "Geography",data = churn_data).set(xlabel='')
-plt.gca().legend().remove()
-
-sns.boxplot(y="EstimatedSalary",x = "Geography", hue = "Geography",data = churn_data).set(xlabel='')
-plt.gca().legend().remove()
+sns.countplot(data=churn_data_v2, x="Exited", hue="Geography")
 
 #Att question An: what are we trying to show with these boxplots? Is there a more illustrative way of showing smth?
 
@@ -228,8 +203,7 @@ plt.gca().legend().remove()
 
 
 ### Influence of other variables on churn --> je ne comprends pas ce que tu veux faire?
-sns.countplot(data=churn_data, x="Exited", hue="Gender") #Refaire ça avec les variables au début (avec female et male plutôt)
-sns.countplot(data=churn_data, x="Exited", hue="Geography")
+sns.countplot(data=churn_data_v2, x="Exited", hue="Gender") #Refaire ça avec les variables au début (avec female et male plutôt)
 
 #Higher churn rate for female compared to male
 #Higher churn rate in Germany and spain compared to france, maybe a link with the number of clients
@@ -241,12 +215,12 @@ sns.countplot(data=churn_data, x="Exited", hue="Geography")
 from scipy.stats import ttest_ind
 
 #Separate the churn variable according to gender and geography
-female_data = churn_data[churn_data["Gender"] == 1]["Exited"]
-male_data = churn_data[churn_data["Gender"] == 0]["Exited"]
+female_data = churn_data_v2[churn_data_v2["Gender"] == 1]["Exited"]
+male_data = churn_data_v2[churn_data_v2["Gender"] == 0]["Exited"]
 
-france_data= churn_data[churn_data["Geography"] == "France"]["Exited"]
-spain_data= churn_data[churn_data["Geography"] == "Spain"]["Exited"]
-germany_data= churn_data[churn_data["Geography"] == "Germany"]["Exited"]
+france_data= churn_data_v2[churn_data_v2["Geography"] == "France"]["Exited"]
+spain_data= churn_data_v2[churn_data_v2["Geography"] == "Spain"]["Exited"]
+germany_data= churn_data_v2[churn_data_v2["Geography"] == "Germany"]["Exited"]
 
 #For gender
 t_statistic, p_value = ttest_ind(female_data, male_data)
@@ -276,52 +250,24 @@ print(f'P-value: {p_value}')
 from statsmodels.discrete.discrete_model import Probit
 from statsmodels.tools.tools import add_constant
 
-churn_data["Geography"] = churn_data["Geography"].replace({"France": 1, "Spain": 2, "Germany":3})
-
+churn_data_v2["Geography"] = churn_data_v2["Geography"].replace({"France": 1, "Spain": 2, "Germany":3})
 
 #print(churn_data.head())
 #churn_data.describe()
 
-Y = churn_data["Exited"]
-X = churn_data.drop(["Exited"], 1)
+Y = churn_data_v2["Exited"]
+X = churn_data_v2.drop(["Exited"], 1)
 X = add_constant(X) #Att why this?
 model = Probit(Y, X.astype(float))
 probit_model = model.fit()
 print(probit_model.summary())
 
 
-
-
-
-
 ### Machine learning Model ###
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-
-#Without the country variable : 
-#An: Why?
-
-Y = churn_data["Exited"]
-X = churn_data.drop("Exited", axis = 1)
-X = X.drop("Geography", axis = 1)
-
-#Spliting the sample
-#J'ajoute stratify
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.3, random_state = 42, stratify=Y)
-
-#looping for best values of min_samples_split 
-
-accuracy_values= []
-
-for i in range(2, 50): 
-    model = RandomForestClassifier(min_samples_split=i, random_state = 42)
-    model.fit(X_train, Y_train)
-    prediction_Y = model.predict(X_test)
-    accuracy = accuracy_score(Y_test, prediction_Y)
-    accuracy_values.append(accuracy)
-
-
+from sklearn.model_selection import GridSearchCV
 
 #With the country variable, using one-hot encoding 
 #Creating a new dataset to not conflict with the previous one
@@ -341,50 +287,23 @@ X = df.drop("Exited", axis = 1)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.3, random_state = 42, stratify = Y)
 
-#looping for best min_samples_split
-accuracy_values = []
+#Grid search to find best parameters for RF 
 
-for i in range(2, 50): 
-    model = RandomForestClassifier(min_samples_split=i, random_state = 42)
-    model.fit(X_train, Y_train)
-    prediction_Y = model.predict(X_test)
-    accuracy = accuracy_score(Y_test, prediction_Y)
-    accuracy_values.append(accuracy)
+model = RandomForestClassifier(random_state=42)
+param_grid = {
+    'n_estimators': list(range(100, 120)),
+    'min_samples_split': list(range(15, 50))}
 
-#accury over different values of min_samples_split 
+grid_search = GridSearchCV(model, param_grid=param_grid, cv=5, n_jobs=12)
+grid_search.fit(X_train, Y_train)    
+best_model = grid_search.best_estimator_
+Y_predicted = best_model.predict(X_test)
+accuracy_best = accuracy_score(Y_test, Y_predicted)
 
-min_samples_split=list(range(2,50))
-plt.plot(min_samples_split, accuracy_values, marker='o')
-plt.title("Change in mse with different min samples split values")
-plt.xlabel("min_sample_split")
-plt.ylabel("accuracy")
-plt.grid(True)
-
-#looping for best n_estimator
-accuracy_values = []
-
-for i in range(80, 200): 
-    model = RandomForestClassifier(n_estimators = i, min_samples_split=27, random_state = 42)
-    model.fit(X_train, Y_train)
-    prediction_Y = model.predict(X_test)
-    accuracy = accuracy_score(Y_test, prediction_Y)
-    accuracy_values.append(accuracy)
-
-#accuracy across n_estimator with best min_samples_split
-
-n_regressors=list(range(80,200))
-plt.plot(n_regressors, accuracy_values, marker='o')
-plt.title("Change in mse with different min samples split values")
-plt.xlabel("n_regressors")
-plt.ylabel("accuracy")
-plt.grid(True)
+print("Best Parameters:", grid_search.best_params_)
+print(f'Accuracy (Best Model): {accuracy_best:.4f}')
 
 
-#1) Best value for min_samples_split is 42, Accuracy = 0.86
-#2) Best value for min_samples_split is 27, best value for n_estimators is 120, Accuracy = 0.875
-#Ici je suis très con parce que je me suis cassé la tete à faire 4 loops alors que ca se résume à deux grid search, il faut que je modifie ca
-
-#With the country variable as a numerical label 
 
 
 
